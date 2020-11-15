05b0c783f8cbebfb1eb817ca091fbf910ac9dcf3
rishab2890 <56164826+rishab2890@users.noreply.github.com>
2020-11-15 11:01:50 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,22 @@
 
+import 'package:flutter/material.dart';
+
+import 'pages/home.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme:
+          ThemeData(primaryColor: Colors.deepPurple, accentColor: Colors.teal),
+      title: 'SocialSlate',
+      home: Home(),
+    );
+  }
+}
+vfefbtnrgnr
